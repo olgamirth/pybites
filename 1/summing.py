8295b788eec4d5c4be486f49f1396613f9008428
Michael Dahlberg <dahlberg@recursoft.org>
2024-09-16 08:41:06 -0400

    Notes for Pybites 1 and First attempt at Pybites 5

--- 1/summing.py
+++ 1/summing.py
@@ -1,4 +1,13 @@
 def sum_numbers(numbers=None):
+    """
+    Learned:
+    (1) The limits of list comprehensions
+    (2) correct usage of range() function
+    (3) How if-else works
+    (4) sum() function
+    (5) basic use of pytest
+
+    """
     result = 0
     if numbers is None:
         result = sum(range(1, 101))
--- 5/names.py
+++ 5/names.py
@@ -1,24 +1,38 @@
+import typer
+
 NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
 
+app = typer.Typer()
 
+
+@app.command()
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    pass
+    seen = set()
+    titled_deduped_names = [name.title() for name in names if not
+                            (name.lower() in seen or seen.add(name.lower()))]
+    return titled_deduped_names
 
 
+@app.command()
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
+    sorted_names = sorted(names, key=lambda x: x.split()[-1])
+    return sorted_names
 
 
+@app.command()
 def shortest_first_name(names):
     """Returns the shortest first name (str).
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    # ...
+    first_names = [name.split()[0]for name in names]
+    return min(first_names, key=len)
+
+
